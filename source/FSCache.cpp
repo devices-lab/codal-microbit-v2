0ba15fc9d42796672637795d5d527e3d2d6f3a2a
KierPalin <45743174+KierPalin@users.noreply.github.com>
2025-11-13 18:49:45 +0000

    FSCache.cpp: cachePage; no longer does flash.read at end, MicroBitLog.h: increased CONFIG_MICROBIT_LOG_CACHE_BLOCK_SIZE

--- source/FSCache.cpp
+++ source/FSCache.cpp
@@ -241,7 +241,8 @@ CacheEntry* FSCache::cachePage(uint32_t address)
 	if (lru->page == NULL)
 		lru->page = (uint8_t *) malloc(blockSize);
 
-	flash.read((uint32_t *)lru->page, address, blockSize / 4);
+	// NOTE: THIS FLASH READ IS UNNCESSARY IN OUR CASE BECAUSE WE ERASE ONCE AT THE START.
+	// flash.read((uint32_t *)lru->page, address, blockSize / 4);
 
 	return lru;
 }
